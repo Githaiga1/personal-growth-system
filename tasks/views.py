ebb64eec7d3286856eaa272f805b95dde829fbcd
B.Githaiga <briangithaiga7452@gmail.com>
2026-07-15 19:26:42 +0300

    Add user signup with UserCreationForm, auto-login, and navigation links

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Task
 from .forms import TaskForm
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('task_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'registration/signup.html', {'form': form})
 
 
 
